93052b46108892974df6b5c86d305eda9d275e9c
Cyanox62 <cyanox62@gmail.com>
2022-02-20 22:00:00 -0500

    Add project files.

---
+++ VCPatch/Plugin.cs
@@ -0,0 +1,52 @@
+﻿using Exiled.API.Features;
+using Exiled.API.Interfaces;
+using HarmonyLib;
+using System;
+using UnityEngine;
+
+namespace VCPatch
+{
+    public class Config : IConfig
+	{
+        public bool IsEnabled { get; set; } = true;
+	}
+
+    public class Plugin : Plugin<Config>
+    {
+        private Harmony hInstance;
+
+		public override void OnEnabled()
+		{
+			base.OnEnabled();
+
+			hInstance = new Harmony("cyan.vcpatch");
+			hInstance.PatchAll();
+		}
+
+		public override void OnDisabled()
+		{
+			base.OnDisabled();
+
+			hInstance.UnpatchAll(hInstance.Id);
+			hInstance = null;
+		}
+
+		public override string Name => "VcPatch";
+		public override string Author => "Cyanox & Mitzey";
+	}
+
+	[HarmonyPatch(typeof(Mirror.NetworkConnection), nameof(Mirror.NetworkConnection.TransportReceive))]
+	public class VCBugPatch
+	{
+		public static bool Prefix(Mirror.NetworkConnection __instance, ArraySegment<byte> buffer)
+		{
+			if (buffer.Count < 2)
+			{
+				Debug.LogError(string.Format("[PATCHED] ConnectionRecv {0} Message was too short (messages should start with message id)", __instance));
+				//this.Disconnect();
+				return false;
+			}
+			return true;
+		}
+	}
+}
